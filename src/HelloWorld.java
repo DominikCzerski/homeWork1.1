9af17f63deadbf704af096e707ec1550deb30837
Dominik <dominik.czerski@monogo.pl>
2020-06-01 22:02:34 +0200

    Added sout "Uczę się programowania"

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,6 +3,17 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Uczę\n" +
                 "się\n" +
-                "programowania");
+                "programowania\n" +
+                "i\n" +
+                "commitowania");
+
+
+       /*
+       I can do this this way also
+       System.out.println("Uczę");
+        System.out.println("się");
+        System.out.println("programowania");
+        System.out.println("i");
+        System.out.println("commitowania");*/
     }
 }
