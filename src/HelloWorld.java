553deb5ff9652966bdc06de45b0d141fc2a8d508
Dominik <dominik.czerski@monogo.pl>
2020-06-01 22:05:49 +0200

    Commented 1 implementation

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,18 +1,18 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Uczę\n" +
+        /*System.out.println("Uczę\n" +
                 "się\n" +
                 "programowania\n" +
                 "i\n" +
                 "commitowania");
-
-       /*
-       I can do this this way also
-       System.out.println("Uczę");
+*/
+        System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
         System.out.println("i");
-        System.out.println("commitowania");*/
+        System.out.println("commitowania");
+
+
     }
 }
