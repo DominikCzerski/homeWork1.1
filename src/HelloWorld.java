bcda91a094ccc4ac2d593ba68814d83690b52b50
Dominik <dominik.czerski@monogo.pl>
2020-06-01 22:07:12 +0200

    Uncommented 1 implementation. HomeWork finished.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,13 +1,14 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        /*System.out.println("Uczę\n" +
+        System.out.println("Uczę\n" +
                 "się\n" +
                 "programowania\n" +
                 "i\n" +
                 "commitowania");
-*/
-     /*   System.out.println("Uczę");
+
+        /* This way is also possible
+        System.out.println("Uczę");
         System.out.println("się");
         System.out.println("programowania");
         System.out.println("i");
